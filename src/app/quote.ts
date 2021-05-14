bab5ff8d79c24d5c1a64037033db753c33440ed1
layersony <sammaingi5@gmail.com>
2021-05-14 20:15:35 +0300

    goal class properties add

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,14 @@
 export class Quote {
-
+  showquotes!:boolean;
+  constructor(
+    public id: number,
+    public title: string,
+    public author: string,
+    public upvote: number,
+    public downvote: number,
+    public submitby: string,
+    public dateupload: Date,
+  ){
+    this.showquotes = false;
+  }
 }
