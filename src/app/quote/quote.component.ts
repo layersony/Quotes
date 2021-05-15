834ddcc8c3e0477003c4c5af10b90b5ef4807e73
layersony <sammaingi5@gmail.com>
2021-05-15 11:48:12 +0300

    quote details display on click

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,8 @@ export class QuoteComponent implements OnInit {
 
   quotearr: Quote[] = [
     new Quote(1, 'trial', 'long list of trial', 33, 20, 'Maingi', new Date(2020,1,1)),
-    new Quote(2, 'trial2', 'long list of trial2', 75, 12, 'Maingi', new Date(2020,1,1))
+    new Quote(2, 'trial2', 'long list of trial2', 75, 12, 'Maingi', new Date(2020,1,1)),
+    new Quote(3, 'trial3', 'long list of trial3', 75, 12, 'Maingi', new Date(2020,1,1))
 
   ]
 
@@ -19,4 +20,8 @@ export class QuoteComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showDetail(index:number){
+    this.quotearr[index].showquotes = !this.quotearr[index].showquotes;
+    console.log(this.quotearr[index])
+  }
 }
