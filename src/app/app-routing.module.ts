a8a0ffef7100ef974603f88fa09f1e69643e079d
layersony <sammaingi5@gmail.com>
2021-05-15 12:31:16 +0300

    Routing create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'addquote',
+    component: QuoteFormComponent,
+  },
+  {
+    path: 'home',
+    component: QuoteComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
